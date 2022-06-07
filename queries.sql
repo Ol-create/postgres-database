43792016635e2059ecc7e980e94a828753c5995e
Oluyemi Paul <oluola96@gmail.com>
2022-06-06 19:13:28 -0600

    feat: neutered animals and less than 3 escape

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 /* List the name of all animals born between 2016 and 2019 */
 SELECT name FROM animals WHERE date_of_birth >= '01-01-2016' AND date_of_birth <= '01-01-2019';
+
+/* List the name of all animals that are neutered and have less than 3 escape attempts. */
+SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts < 3;
