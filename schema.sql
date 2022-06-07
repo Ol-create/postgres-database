a4a62b0291eb95ed29a65f753e9a42bda3a8ad05
Oluyemi Paul <oluola96@gmail.com>
2022-06-07 04:14:46 -0600

    fix: change name data type to varchar(225);

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (id SERIAL, 
-                      name TEXT, 
+                      name VARCHAR(255), 
                       date_of_birth DATE, 
                       escape_attempts INT, 
                       neutered BOOLEAN, 
