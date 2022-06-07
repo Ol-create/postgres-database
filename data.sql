3b9a48132f274d882d79c317d8a7661e715d6b69
Oluyemi Paul <oluola96@gmail.com>
2022-06-07 04:31:05 -0600

    feat: track data for backup

--- data.sql
+++ data.sql
@@ -5,3 +5,5 @@ VALUES ('Agumon', '3-2-2020', 0, true, 10.23),
        ('Gabumon', '15-11-2018', 2, true, 8), 
        ('Pikachu', '7-1-2021', 1, false, 15.04), 
        ('Devimon', '12-5-2017', 5, true, 11);
+/* Track data for backup */
+  BEGIN TRANSACTION;
