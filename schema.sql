56ad704826afded7d8ec88598a6993b4bb3b3cf1
Oluyemi Paul <oluola96@gmail.com>
2022-06-07 04:27:46 -0600

    feat: track data for backup

--- schema.sql
+++ schema.sql
@@ -1,8 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (id SERIAL, 
-                      name VARCHAR(255), 
-                      date_of_birth DATE, 
-                      escape_attempts INT, 
-                      neutered BOOLEAN, 
-                      weight_kg NUMERIC);
+CREATE TABLE animals 
+  (id SERIAL, 
+  name VARCHAR(255), 
+  date_of_birth DATE, 
+  escape_attempts INT, 
+  neutered BOOLEAN, 
+  weight_kg NUMERIC);
+   
+  /* Track data for backup */
+  BEGIN TRANSACTION;
