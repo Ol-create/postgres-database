af7ce54fac0b6d2daf65aba199e114f3b9904956
Oluyemi Paul <oluola96@gmail.com>
2022-06-08 09:54:09 -0600

    Feat: update all weight to positive values

--- data.sql
+++ data.sql
@@ -8,14 +8,30 @@ VALUES ('Agumon', '3-2-2020', 0, true, 10.23),
        
 /* Start transaction; */
 BEGIN TRANSACTION;
+
 /* Update species record with digimon */
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
 /* Update the remaining species record with pokemon */
 UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+
 /* Save transaction */
 COMMIT TRANSACTION;
 BEGIN TRANSACTION;
+
 /* Delete all records in the animals table, then roll back the transaction */
 DELETE FROM animals;
 ROLLBACK;
+BEGIN TRANSACTION;
 
+/* Delete all animals born after Jan 1st, 2022. */
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SAVEPOINT jan_2022;
+
+/* Update all animals' weight to be their weight multiplied by -1 */
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK jan_2022;
+
+/* Update all animals' weights that are negative to be their weight multiplied by -1. */
+UPDATE animals SET weight_kg = weight_kg* -1 WHERE weight_kg <0;
+COMMIT TRANSACTION;
