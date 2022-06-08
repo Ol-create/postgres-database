998c44abe843c28f3255afd6b8fd783b80d3fcdf
Oluyemi Paul <oluola96@gmail.com>
2022-06-08 12:21:23 -0600

    Feat: Insert date into species table

--- data.sql
+++ data.sql
@@ -60,3 +60,7 @@ VALUES ('Sam Smith', 34),
        ('Melody Pond', 77), 
        ('Dean Winchester', 14), 
        ('Jodie Whittaker', 38);
+
+/* Insert date into species table */
+INSERT INTO species (name) 
+VALUES ('Pokemon'), ('Digimon');
