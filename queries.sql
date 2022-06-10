e5484907eb39b813fbc3ae13aa96c5ce9f556b37
Oluyemi Paul <oluola96@gmail.com>
2022-06-10 10:39:22 -0600

    Who was Maisy Smith's first visit

--- queries.sql
+++ queries.sql
@@ -100,3 +100,18 @@ SELECT animals.name, COUNT(*) as count FROM animals
   GROUP BY animals.name
   ORDER BY count DESC
   LIMIT 1;
+
+-- Who was Maisy Smith's first visit?
+SELECT animals.name FROM visits 
+  JOIN vets ON vets.id = visits.vets_id
+  JOIN animals ON animals.id = visits.animals_id
+  WHERE vets.name = 'Maisy Smith'
+  ORDER BY visits.date_of_visit
+  LIMIT 1;
+  
+  -- Details for most recent visit: animal information, vet information, and date of visit.
+SELECT * FROM visits
+	JOIN animals ON animals.id = visits.animals_id
+	JOIN vets ON vets.id = visits.vets_id
+	ORDER BY date_of_visit
+	LIMIT 1;
